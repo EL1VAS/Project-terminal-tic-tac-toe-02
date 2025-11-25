f2ca2dd46a35319874538289eb230035b4c7728f
Pythia <el1vas@yahoo.gr>
2025-11-25 22:13:27 +0100

    Wrote the function for an array of arrays and not a flat array

--- board-printer.js
+++ board-printer.js
@@ -18,15 +18,26 @@
 export function printBoard(board) {
   console.clear(); // Clears the console before each log
   console.log("\n****** TIC TAC TOE GAME******\n");
-  console.log(`${board[0]} || ${board[1]} || ${board[2]}`); // Logs first row
-  console.log(">>>>>>>>>>>");
-  console.log(`${board[3]} || ${board[4]} || ${board[5]}`); // Logs second row
-  console.log("<<<<<<<<<<<");
-  console.log(`${board[6]} || ${board[7]} || ${board[8]}`); // Logs third row
+  for (let row = 0; row < board.length; row++) {
+    console.log(" " + board[row].join("||") + " ");
+    if (row < board.length - 1) {
+      console.log("=============");
+    }
+  }
 }
-const testBoard = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const testBoard = [
+  ["X", "_", "O"],
+  ["O", "_", "X"],
+  ["_", "X", "O"],
+];
 printBoard(testBoard);
-const testTwoBoard = ["_", "_", "_", "_", "_", "_", "_", "_", "_"];
+
+const testTwoBoard = [
+  ["_", "_", "_"],
+  ["_", "_", "_"],
+  ["_", "_", "_"],
+];
 printBoard(testTwoBoard);
 /*
     Given a tic-tac-toe board (an array of arrays),
@@ -34,17 +45,28 @@ printBoard(testTwoBoard);
         - return false if there are still moves that can be made
 */
 export function checkIfNoMovesLeft(board) {
-  for (let i = 0; i < board.length; i++) {
-    // Checking each index of the board array
-    if (board[i] === "_") {
-      // If in the index there is a "_" it means we still got moves
-      return false; // So false on the statement "No moves left"
+  for (let row = 0; row < board.length; row++) {
+    // Where row is the index+1 of our array of arrays
+    for (let column = 0; column < board[row].length; column++) {
+      // Where column is index+1 of the elements of each array inside the main array
+      if (board[row][column] === "_") {
+        // Searches by coordinators and find an empty cell
+        return false; // So there are still moves to do
+      }
     }
   }
-  return true; // True if the loop doesn't find any "_"
+  return true; // No more moves
 }
 
-const testNotFullBoard = ["X", "_", "O", "_", "X", "_", "_", "O", "O"];
+const testNotFullBoard = [
+  ["_", "_", "O"],
+  ["O", "_", "X"],
+  ["_", "O", "O"],
+];
 console.log(checkIfNoMovesLeft(testNotFullBoard));
-const testFullBoard = ["X", "X", "O", "X", "X", "O", "O", "O", "O"];
+const testFullBoard = [
+  ["X", "X", "O"],
+  ["O", "O", "X"],
+  ["X", "X", "O"],
+];
 console.log(checkIfNoMovesLeft(testFullBoard));
